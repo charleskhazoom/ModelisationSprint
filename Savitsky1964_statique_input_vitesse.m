cb1eea3a51b6f912007e2650ad2e0c30d85b3057
charleskhazoom <charles.khazoom@hotmail.com>
2016-07-20 20:33:59 +0200

    Delete old unuseful files.
    Adapter pour utiliser la méthode de la bissection seulement.

--- Savitsky1964_statique_input_vitesse.m
+++ Savitsky1964_statique_input_vitesse.m
@@ -2,65 +2,25 @@
 % Selon Savitsky 1964
 % Par Charles Khazoom
 clear;close all;clc
+tic
 %% Données
-% m = 600/2.2;%60000/2.2; % kg
-% L = 4.19; % Longeur du bateau en m
-% LCG = 0.3*L;%29*12*2.54/100; % m LCG = position du CG (bateau au complet) à partir de la poupe (arrière du bateau)
-% VCG = 0.15;%2*12*2.54/100; % m ; VCG = ??
-% b = 0.5;%14*12*2.54/100; % m ; b = beam
-% beta = 0;%30;%10;% degrés ; deadrise angle
 v = 22.2633;%50*1.6/3.6;%40*0.514444; % m/s (1 noeud = 0.514444 m/s); vitesse d'avance du bateau
-% a = VCG - (b/4)*tand(beta);
-% % a = 1.39*12*2.54/100;
-% f = 0.1;%0.5*12*2.54/100;
-% epsilon = 0; % degrés
 %% Ajouter les variables globales
 GlobalHullGeometry; % load hull geometry
 global m L LCG VCG b beta a f epsilon
-% [m,L,LCG,VCG,b,beta,a,f,epsilon] = unwrap_geodata(geodata);
 global g
 g= 9.81;% m/s2, accélération gravitationnelle
 global rho
 rho = 1000; % kg/m3; eau douce = 1000; eau salée = 1025
-methode = 'biss';
-% methode = 'it';
 %% Planing coefficients
 % Cv = v/sqrt(g*b);
-% CL_beta = m*g/(0.5*rho*v^2*b^2);% lift coefficient pour un angle beta. 
-% Déjà calculés dans la fonction 
+% CL_beta = m*g/(0.5*rho*v^2*b^2);% lift coefficient pour un angle beta.
+% Déjà calculés dans la fonction statique_input_vitesse_v2
 %% Calculs
-tic
-tau = [1:.01:12];%[2 4 6 10 12 15]%
-if strcmp(methode,'biss')
-%     [~,~,~,~,~,c(1),Df(1)] =  statique_input_vitesse_v2(m,LCG,VCG,b,beta,v,a,f,epsilon,tau(1));
-%     [~,~,~,~,~,c(2),Df(2)] =  statique_input_vitesse_v2(m,LCG,VCG,b,beta,v,a,f,epsilon,tau(end));
-elseif strcmp(methode,'it')
-    Residus = zeros(size(tau));
-    for ii = 1:length(tau)
-        [Residus(ii),lambda(ii),D(ii),CL_beta,Cv,c(ii),Df(ii)] =  statique_input_vitesse_v2(v,tau(ii));
-    end
-end
-%%
-if strcmp(methode,'biss')
-y{1} = @(tau0,c,Df) m*g*((1-sind(tau0)*sind(tau0+epsilon))*c/cosd(tau0)-f*sind(tau0)) + Df*(a-f);
-y{2} = @statique_input_vitesse_v2;
-A = tau(1);
-B = tau(end);
-racine_bissection = bissection_fcn(A,B,y,0.01,v);
-tau_e = racine_bissection;
-end
-%% Solution pour laquelle eq35 = 0
-if strcmp(methode,'it')
 
-% figure;plot(Residus,tau)
-tau_e = interp1(Residus,tau,0); % tau à l'équilibre.
-lambda_e = interp1(tau,lambda,tau_e); % lambda à l'équilibre
-D_e = interp1(tau,D,tau_e);% Drag horizonal D à l'équilibre
-P = D_e*v; % Puissance à l'état d'équilibre
-
-[r,l,d] = statique_input_vitesse(v,tau_e);
-end
+tau = [1 12]; % Fenêtre pour la méthode de la bissection.
+tau_e = bissection_fcn(tau(1),tau(end),@statique_input_vitesse,0.01,v);
+toc
 %% Porpoising Stability
 
 % CL_beta
-toc
--- statique_input_vitesse.m
+++ statique_input_vitesse.m
@@ -1,6 +1,7 @@
 function [Residus,lambda,D,CL_beta,Cv,c,Df] = statique_input_vitesse(v,tau)
 global g
 global rho
+global m L LCG VCG b beta a f epsilon
 %%%% Planing coefficients
 Cv = v/sqrt(g*b);
 CL_beta = m*g/(0.5*rho*v^2*b^2);% lift coefficient pour un angle beta.
@@ -29,23 +30,6 @@ elseif isempty(lambda)
 end
 
 %%%% Calcul de vm
-% if beta == 0;
-%     vm = v*sqrt(1-0.0120*tau^1.1/sqrt(lambda)/cosd(tau)); %%%%% Only if beta == 0;
-% if beta == 10
-%     switch tau
-%         case 2
-%             vm = 67.0*12*2.54/100;
-%         case 3
-%             vm = 66.6*12*2.54/100;
-%         case 4
-%             vm = 66.2*12*2.54/100;
-%         otherwise
-%             vm = (67 + 66.6)/2*12*2.54/100;
-%     end
-% else % if beta == 0;
-%     vm = v*sqrt(1-0.0120*tau^1.1/sqrt(lambda)/cosd(tau)); %%%%% Only if beta == 0;
-% end
-%     vm = v*sqrt(1-0.0120*tau^1.1/sqrt(lambda)/cosd(tau)); %%%%% Only if beta == 0;
 vm = v*sqrt(1 - (0.0120*sqrt(lambda)*tau^1.1 -...
     0.0065*beta*(0.0120*sqrt(lambda)*tau^1.1)^0.6)/lambda/cosd(tau));
 
@@ -65,35 +49,19 @@ Cf = 0.075/((log10(Re)-2)^2); %%%% L'auteur propose  Schoenherr Cf formula,
 delta_Cf = 0.0004 ;% ATTC Standard Roughness
 
 %%%% Calcul Df, (frictional drag)
-% Df = rho*vm^2*lambda*b^2*(Cf+delta_Cf)/2/cosd(beta);
-% switch tau
-%     case 2
-%         Df = 7340/2.2*g;
-%     case 3
-%         Df =    5160/2.2*g;
-%     case 4
-%         Df =3760/2.2*g;
-%     otherwise
-        Df = rho*vm^2*lambda*b^2*(Cf+delta_Cf)/2/cosd(beta);
-% end
+Df = rho*vm^2*lambda*b^2*(Cf+delta_Cf)/2/cosd(beta);
 
 %%%% Calcul D (Drag total)
-D = Df/cosd(tau) + m*g*tand(tau); % Ce calcul du drag néglige le lift 
-% généré par le trust, qui peut avoir une composante verticale.
-% N = m*g*(1-sind(tau)*sind(tau+epsilon))/cosd(tau);
-% D2 = Df*cosd(tau)+N*sind(tau)
+T = (m*g*sind(tau)+Df)/cosd(epsilon);
+N = (m*g + Df*sind(tau)-T*sind(tau+epsilon))/cosd(tau);
+D = Df*cosd(tau)+N*sind(tau);
+
 %%%% Calcul de c
 Cp = 0.75 - 1/(5.21*Cv^2/lambda^2+2.39);
 lp = Cp*lambda*b;
 c = LCG - lp;
 
-
 %%%% Équation 35 de Savitsky1964 à mettre égale à 0.
-eq35 = @(m,g,tau,epsilon,a,c,f) m*g*((1-sind(tau)*sind(tau+epsilon))*c/cosd(tau)-f*sind(tau)) + Df*(a-f);
-Residus =  eq35(m,g,tau,epsilon,a,c,f);
-
-% Residus = m*g*((1-sind(tau)*sind(tau+epsilon))*c/cosd(tau)-f*sind(tau)) + Df*(a-f); %%%%%%%% quantité à mettre égale à 0;
-
-
+Residus = N*c + Df*a-T*f;
 end
 
